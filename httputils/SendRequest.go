ca4684190756b74134654d842dff2364cda132b5
sohenk <258601119@qq.com>
2023-03-22 15:13:42 +0800

    feat: add my utils

---
+++ httputils/SendRequest.go
@@ -0,0 +1,30 @@
+package httputils
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+// 发送请求
+func SendRequest(method string, url string, body []byte) ([]byte, error) {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return bodyBytes, nil
+}
