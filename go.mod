2b62e6f280e9516ec2a026f78231e0e0d7bd03ef
sohenk <258601119@qq.com>
2023-04-23 21:19:15 +0800

    update new function

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/sohenk/so-go-helper
 go 1.18
 
 require (
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/gogf/gf v1.16.9
 	golang.org/x/crypto v0.7.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 )
--- httputils/SendRequest.go
+++ httputils/SendRequest.go
@@ -3,18 +3,23 @@ package httputils
 import (
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 // 发送请求
-func SendRequest(method string, url string, body []byte) ([]byte, error) {
+func SendRequest(method string, url string, body []byte, timeout time.Duration) ([]byte, error) {
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
+	if timeout == 0 {
+		timeout = time.Millisecond * 200
+	}
+	client := &http.Client{
+		Timeout: timeout,
+	}
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
